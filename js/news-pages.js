7e646bd4d570c996f2aaf5a22f7a23e2da3fc358
magmahindenburg <magma@moar.se>
2015-05-14 17:45:49 +0900

    Added price chart on top of new page

--- js/news-pages.js
+++ js/news-pages.js
@@ -1,6 +1,18 @@
 var country;
 var featuredExchange;
-var availableExchanges = ['bitstamp', 'kraken'];
+var availableExchanges = ['bitstamp', 'kraken', 'bitflyer'];
+var exchangeUrl;
+
+Number.prototype.formatMoney = function(c, d, t){
+	var n = this, 
+	    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+	    d = d == undefined ? "." : d, 
+	    t = t == undefined ? "," : t, 
+	    s = n < 0 ? "-" : "", 
+	    i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", 
+	    j = (j = i.length) > 3 ? j % 3 : 0;
+	   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
 
 $(document).ready(function(){
 	
@@ -15,13 +27,54 @@ $(document).ready(function(){
 	            	
 	            	country = response.countryCode;
 	            	featuredExchange = response.sponsoredListing;
+	            	var useNext = false;
 	            	
-	            	// If exchange does not exist
 	            	if (availableExchanges.indexOf(featuredExchange) == -1)
 	            	{
-	            		featuredExchange = 'bitstamp';
+	            		useNext = true;
 	            	}
 	            	
+	            	for (var i=0; i<response.localListings.length; i++)
+	            	{
+	            		if (useNext && availableExchanges.indexOf(response.localListings[i].id) > -1)
+	            		{
+	            			featuredExchange = response.localListings[i].id;
+	            			exchangeUrl = response.localListings[i].homepageURL;
+	            			useNext = false;
+	            		}
+	            		if (response.sponsoredListing == response.localListings[i].id)
+	            		{
+	            			exchangeUrl = response.localListings[i].homepageURL;
+	            		}
+	            	}
+	            	
+	            },
+	            500: function (response) {
+	            	console.log(response);
+	            }
+	          },
+	          complete: function(e, xhr, settings){
+	        	  getTicker();
+	        	  setInterval(function(){ getTicker(); }, 60000);
+	          }
+	});
+	
+	$.ajax({
+	    url: "/api/ticker/30dayUSD",
+	    type: "GET",
+	    cache: false,
+	    data: { },
+	    statusCode: {
+	            200: function (response) {
+	            	console.log(response);
+	            	
+	            	var graphData = [];
+	            	
+	            	for (var i=0; i<response.data.length; i++)
+	            	{
+	            		graphData[graphData.length] = response.data[i][1];
+	            	}
+	            	drawChart(graphData);
 	            },
 	            500: function (response) {
 	            	console.log(response);
@@ -45,9 +98,9 @@ function getTicker() {
 	            200: function (response) {
 	            	console.log(response);
 	            	
-	            	var last = parseFloat(response.last).toFixed(2);
-	            	var high = parseFloat(response.high).toFixed(2);
-	            	var low = parseFloat(response.low).toFixed(2);
+	            	var last = parseFloat(response.last);
+	            	var high = parseFloat(response.high);
+	            	var low = parseFloat(response.low);
 	            	var currency = response.currency;
 	            	var change = ( parseFloat(response.vwap) / parseFloat(response.high) -1 ) * 100;
 	            	var triangle = ' ▼';
@@ -55,24 +108,39 @@ function getTicker() {
 	            	
 	            	if (response.currency == 'USD')
 	            	{
-	            		$('.news-header-bitcoin-price div.quote').html('$' + last);
-	            		$('.news-header-bitcoin-price span.daily-high').html('HI: $' + high);
-	            		$('.news-header-bitcoin-price span.daily-low').html('LOW: $' + low);
+	            		$('.news-header-bitcoin-price div.quote').html('$' + last.formatMoney(2, '.', ' '));
+	            		$('.news-header-bitcoin-price span.daily-high').html('HI: $' + high.formatMoney(2, '.', ','));
+	            		$('.news-header-bitcoin-price span.daily-low').html('LOW: $' + low.formatMoney(2, '.', ','));
 	            	} else if (response.currency == 'EUR') {
-	            		$('.news-header-bitcoin-price div.quote').html(last + '€');
-	            		$('.news-header-bitcoin-price span.daily-high').html('HI: ' + high + '€');
-	            		$('.news-header-bitcoin-price span.daily-low').html('LOW: ' + low + '€');
+	            		$('.news-header-bitcoin-price div.quote').html(last.formatMoney(2, '.', ' ') + '€');
+	            		$('.news-header-bitcoin-price span.daily-high').html('HI: ' + high.formatMoney(2, '.', ' ') + '€');
+	            		$('.news-header-bitcoin-price span.daily-low').html('LOW: ' + low.formatMoney(2, '.', ' ') + '€');
+	            	} else if (response.currency == 'JPY') {
+	            		$('.news-header-bitcoin-price div.quote').html(last.formatMoney(0, '.', ',') + '円');
+	            		$('.news-header-bitcoin-price span.daily-high').html('HI: ' + high.formatMoney(0, '.', ',') + '円');
+	            		$('.news-header-bitcoin-price span.daily-low').html('LOW: ' + low.formatMoney(0, '.', ',') + '円');
+	            	}
+	            	if (!isNaN(change))
+	            	{
+	            		if (change > 0)
+	            		{
+	            			triangle = ' ▲';
+	            			$('.news-header-bitcoin-price span.change').removeClass('down');
+	            			$('.news-header-bitcoin-price span.change').addClass('up');
+	            		}
+	            		$('.news-header-bitcoin-price span.currency').html(currency + triangle);
+	            		$('.news-header-bitcoin-price span.change').html(change +'%'+ triangle);
+	            	} else {
+	            		$('.news-header-bitcoin-price span.currency').html(currency);
+	            		$('.news-header-bitcoin-price span.change').remove();
 	            	}
 	            	
-	            	if (change > 0)
-            		{
-            			triangle = ' ▲';
-            			$('.news-header-bitcoin-price span.change').removeClass('down');
-            			$('.news-header-bitcoin-price span.change').addClass('up');
-            		}
-	            	$('.news-header-bitcoin-price span.currency').html(currency + triangle);
-            		$('.news-header-bitcoin-price span.change').html(change +'%'+ triangle);
+	            	if ($('.news-header-bitcoin-price div.quote').html().length > 6)
+	            	{
+	            		$('.news-header-bitcoin-price div.quote').css('font-size', '2.5em');
+	            	} // http://bitcoin.com/url?promo=bitflyer-JP&url=https://www.bitflyer.jp/
             		$('div.news-header-ad-unit-300x100 img').attr('src', '/img/news/exchange/' + response.exchange + '.png')
+            		$('div.news-header-ad-unit-300x100 a').attr('href', 'url?promo=' + featuredExchange + '-' + country + '&url=' + exchangeUrl)
 	            },
 	            500: function (response) {
 	            	console.log(response);
@@ -87,4 +155,59 @@ function getTicker() {
 	        	  });
 	          }
 	});
+}
+
+function drawChart(graphData) {
+	
+	var today = new Date();
+	var startDate = new Date().setDate(today.getDate()-30);
+	
+	$('#container').highcharts({
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: null
+        },
+        xAxis: {
+            type: 'datetime',
+            text: null
+        },
+        yAxis: {
+            text: null,
+            title: null
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                    stops: [
+                            [0, '#2c6fad'],
+                            [1, '#2c6fad']
+                        ]
+                },
+                marker: {
+                    radius: 0
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+        series: [{
+            type: 'area',
+            name: 'USD per BTC',
+            pointInterval: 24 * 3600 * 1000,
+            pointStart: startDate,
+            data: graphData
+        }]
+    });
 }
