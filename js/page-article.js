1dad178021c7cac6b1d92769e4ce64540f8b9877
magmahindenburg <magma@moar.se>
2015-06-02 16:59:04 +0900

    Added Tweet and Share on Facebook

--- js/page-article.js
+++ js/page-article.js
@@ -189,6 +189,16 @@ function getArticle(articleId) {
 		      	        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
 		      	        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 		      	    })();
+		      	    
+		      	  // Load FB share button
+		      	  $('.fb-share-button').attr('href', window.location.href);
+		      	  (function(d, s, id) {
+		      	  var js, fjs = d.getElementsByTagName(s)[0];
+		      	  if (d.getElementById(id)) return;
+		      	  js = d.createElement(s); js.id = id;
+		      	  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.3";
+		      	  fjs.parentNode.insertBefore(js, fjs);
+		      	  }(document, 'script', 'facebook-jssdk'));
 
 	        	  linkClicked = false;
 	          }
